5ef926047f5f30bc4f8ad4878fa20945bb00083c
Francisco Sarabia <siglasconexion@gmail.com>
2022-07-18 19:10:52 -0400

    cambio a develop

--- backend.php
+++ backend.php
@@ -3,7 +3,7 @@
        $linki = new mysqli("localhost", "root", "", "censo");
        $linki->query("SET CHARACTER SET utf8");
        if (mysqli_connect_errno()) {
-          die("No se puede conectar a la base de datos:" . mysqli_connect_error());
+          die("No_______ se puede conectar a la base de datos:" . mysqli_connect_error());
        }
 
 
